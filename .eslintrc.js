a71bcbeea1b3488f48588aa607da2a4596b41490
StefanoBichicchi <bichicchi.stefano@gmail.com>
2020-10-20 18:28:09 +0200

    add rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,6 @@ module.exports = {
     'no-console': 'off',
     'vue/max-attributes-per-line': 'off',
     'prettier/prettier': ['error', { semi: false }],
+    'vue/singleline-html-element-content-newline': 'off',
   },
 }
