cf1b75b0423fef86e418b5a30969dbe4e4e31c91
StefanoBichicchi <bichicchi.stefano@gmail.com>
2020-11-05 15:09:36 +0100

    fix db connect

--- src/db/connectToDB.js
+++ src/db/connectToDB.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const db = mongoose.connection
 
-module.exports = function () {
+module.exports = async function () {
   if (db.readyState === 0) {
     db.on('error', console.error.bind(console, 'Errore di connessione:'))
     db.once('open', () => console.info('DB connesso correttamente'))
@@ -11,12 +11,13 @@ module.exports = function () {
     if (process.env.NODE_ENV === 'production')
       connectionUrl = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_IP}:${process.env.DB_PORT}/${process.env.DB_NAME}`
     else
-      connectionUrl = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_IP_DEV}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+      connectionUrl = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_IP_DEV}:${process.env.DB_PORT}/${process.env.DB_NAME_TEST}`
 
-    mongoose.connect(connectionUrl, {
+    await mongoose.connect(connectionUrl, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true,
+      useFindAndModify: false,
       authSource: process.env.DB_AUTH,
     })
   }
