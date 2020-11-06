8eaaa72f3f5ddd0f0828a81b3133b28110cd8c06
StefanoBichicchi <bichicchi.stefano@gmail.com>
2020-11-06 15:39:49 +0100

    add admin page

---
+++ pages/amministrazione.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <v-tabs fixed-tabs align-with-title>
+      <v-tab v-if="$auth.user.ruolo.includes('ADM')">Amministratore</v-tab>
+      <v-tab
+        v-if="
+          $auth.user.ruolo.includes('ADM') || $auth.user.ruolo.includes('DIR')
+        "
+      >
+        Direttivo
+      </v-tab>
+      <v-tab
+        v-if="
+          $auth.user.ruolo.includes('ADM') || $auth.user.ruolo.includes('GA')
+        "
+      >
+        Ambientazione
+      </v-tab>
+      <v-tab
+        v-if="
+          $auth.user.ruolo.includes('ADM') || $auth.user.ruolo.includes('REG')
+        "
+      >
+        Regolamento
+      </v-tab>
+    </v-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: ['auth', 'admin'],
+
+  head() {
+    return {
+      title: 'Amministrazione',
+    }
+  },
+}
+</script>
+
+<style></style>
