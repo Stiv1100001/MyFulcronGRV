77147f3862545fd946d9e01aaf7bf3f730f4821e
StefanoBichicchi <bichicchi.stefano@gmail.com>
2020-11-06 15:39:33 +0100

    add dashboard

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,39 +1,52 @@
 <template>
   <v-row align="start" justify="start">
-    <v-col cols="12" sm="12" md="8" lg="4" xl="4">
-      <v-card elevation class="mb-5">
-        <div class="d-flex flex-no-wrap justify-space-between">
-          <div>
-            <v-card-title>
-              {{
-                $auth.user.anagrafica.nome + ' ' + $auth.user.anagrafica.cognome
-              }}
-            </v-card-title>
-            <v-card-subtitle>
-              Tessera N° {{ $auth.user.n_tessera }}
-            </v-card-subtitle>
-          </div>
-          <div>
-            <v-avatar tile size="125">
-              <v-img :src="profilePic" />
-            </v-avatar>
-          </div>
-        </div>
-      </v-card>
-      <v-card elevation>
-        <v-card-title> Dati giocatore </v-card-title>
-        <v-card-text>
-          <p>PAB Totali: {{ $auth.user.giocatore.pab_totali }}</p>
-          <p>PAB Spendibili: {{ $auth.user.giocatore.pab_spendibili }}</p>
-        </v-card-text>
-      </v-card>
+    <v-col cols="12" sm="12" md="12" lg="8" xl="8">
+      <v-row align="start">
+        <v-col cols="12" sm="12" md="8" lg="6" xl="6">
+          <UserCard id="ucard" ref="ucard" :user="$auth.user" />
+          <PlayerCard id="pcard" ref="pcard" :player="$auth.user.giocatore" />
+        </v-col>
+        <v-col cols="12" sm="12" md="4" lg="6" xl="6">
+          <NewsCard id="ncard" :height="height" />
+        </v-col>
+        <v-col cols="12">
+          <PlayersSlot
+            id="pgcard"
+            :personaggi="$auth.user.giocatore.personaggi"
+          />
+        </v-col>
+      </v-row>
+    </v-col>
+    <v-col cols="12" sm="12" md="12" lg="4" xl="4">
+      <EventSlot id="ecard" class="mt-3" />
     </v-col>
   </v-row>
 </template>
 
 <script>
+import UserCard from '~/components/dashboard/UserCard'
+import PlayerCard from '~/components/dashboard/PlayerCard'
+import NewsCard from '~/components/dashboard/NewsCard'
+
+import PlayersSlot from '~/components/dashboard/PlayersSlot'
+import EventSlot from '~/components/dashboard/EventSlot'
+
 export default {
+  components: {
+    UserCard,
+    PlayerCard,
+    NewsCard,
+    PlayersSlot,
+    EventSlot,
+  },
+
   middleware: ['auth'],
+
+  data() {
+    return {
+      height: null,
+    }
+  },
 
   head() {
     return {
@@ -41,10 +54,9 @@ export default {
     }
   },
 
-  computed: {
-    profilePic() {
-      return this.$auth.user.profileImg ?? '/static/icon.png'
-    },
+  mounted() {
+    const refs = this.$refs
+    this.height = refs.ucard.$el.clientHeight + refs.pcard.$el.clientHeight + 20
   },
 }
 </script>
