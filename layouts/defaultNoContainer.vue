d65b79d1240ffac5752e4498266a43d16e0fb054
StefanoBichicchi <bichicchi.stefano@gmail.com>
2020-11-22 11:38:21 +0100

    add layout

---
+++ layouts/defaultNoContainer.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <NavBarInside />
+    <v-main>
+      <nuxt />
+    </v-main>
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import NavBarInside from '~/components/NavBarInside'
+import Footer from '~/components/Footer'
+
+export default {
+  components: {
+    NavBarInside,
+    Footer,
+  },
+  data() {
+    return {
+      title: 'My FulcronGRV',
+    }
+  },
+}
+</script>
