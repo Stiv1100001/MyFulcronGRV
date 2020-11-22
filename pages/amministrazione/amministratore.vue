d23570a2a9fa6b930abf32624c5daa4549b76cef
StefanoBichicchi <bichicchi.stefano@gmail.com>
2020-11-22 11:36:01 +0100

    created pages

---
+++ pages/amministrazione/amministratore.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container fluid>
+    <v-row align="start" justify="center">
+      <v-col cols="12">
+        <v-banner
+          elevation="10"
+          rounded="lg"
+          single-line
+          icon="fas fa-user"
+          icon-color="primary"
+          color="accent"
+        >
+          Gestisci utenti
+        </v-banner>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true,
+    }
+  },
+}
+</script>
+
+<style></style>
